db7e67b14258636bbea087cc14db72d14837343a
Chamindu Salman <chamindu07@gmail.com>
2017-06-01 18:15:27 +0100

    scraper now converts words into minutes

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,11 +18,11 @@ Recipe.destroy_all
 
 admin = User.create(first_name: "administration", email: "hubert@gmail.com", password: "123456")
 
-1.times do |i|
+2.times do |i|
     url = "https://www.bbcgoodfood.com/search/recipes?query=&op=Search#page=#{i}"
     html = Nokogiri::HTML(open(url))
 
-    ingredients = ["french"]
+    ingredients = ["french", "italian", "sushi"]
     base_url = "https://www.bbcgoodfood.com"
     ingredients.each do |ingredient|
       url = "https://www.bbcgoodfood.com/search/recipes?query=#{ingredient}#query=#{ingredient}&page=#{i}"
