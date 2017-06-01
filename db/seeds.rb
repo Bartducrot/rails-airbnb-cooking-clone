a579e4e5ea74453ab41c21e603c2e0177dd49bd3
Chamindu Salman <chamindu07@gmail.com>
2017-06-01 13:14:30 +0100

    made scraper for index

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 User.destroy_all
 Recipe.destroy_all
 
-bob = User.create(email: "bob@gmail.com", password: "123123")
+admin = User.create(email: "bob@gmail.com", password: "123123")
 
-ingredients = ["salmon", "chocolate", "vanilla", "pasta"]
-
+ingredients = [""]
+base_url = "https://www.bbcgoodfood.com"
 ingredients.each do |ingredient|
   url = "https://www.bbcgoodfood.com/search/recipes?query=#{ingredient}"
   html = Nokogiri::HTML(open(url))
@@ -21,15 +21,19 @@ ingredients.each do |ingredient|
     cooking_time = node.search('.teaser-item__info-item--total-time').text.strip
     difficulty = node.search('.teaser-item__info-item--skill-level').text.strip
     description = node.search('.field-items > .field-item').text.strip
-    photo = node.search('.teaser-item__image img').text.strip
+
+    show = base_url + node.search('a').attr('href').text.strip
+    show_html = Nokogiri::HTML(open(show))
+    photo_max = show_html.search(".img-container img").attr('src').text.strip
 
     recipe = Recipe.new()
     recipe.title = title
     recipe.cooking_time = cooking_time
     recipe.difficulty = difficulty
     recipe.description = description
-    recipe.photo = photo
-    recipe.user = bob
+    recipe.remote_photo_url = "https:" + photo_max if photo_max
+    recipe.user = admin
     recipe.save!
+    puts "successfully created recipe #{recipe.title}"
   end
 end
