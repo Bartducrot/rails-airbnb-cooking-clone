d251bcb9f338d601781fa3f3fd37b4e4aca00f94
Chamindu Salman <chamindu07@gmail.com>
2017-06-01 10:44:50 +0100

    developing scraper

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530163740) do
+ActiveRecord::Schema.define(version: 20170531143859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "recipes", force: :cascade do |t|
-    t.integer "cooking_time"
+    t.string "cooking_time"
     t.string "difficulty"
     t.integer "price"
     t.string "cuisine_type"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Recipe.destroy_all
+
+bob = User.create(email: "bob@gmail.com", password: "123123")
+
+ingredients = ["salmon", "chocolate", "vanilla", "pasta"]
+
+ingredients.each do |ingredient|
+  url = "https://www.bbcgoodfood.com/search/recipes?query=#{ingredient}"
+  html = Nokogiri::HTML(open(url))
+
+  html.search('.node-recipe').each do |node|
+    title = node.search('h3 a').text.strip
+    cooking_time = node.search('.teaser-item__info-item--total-time').text.strip
+    difficulty = node.search('.teaser-item__info-item--skill-level').text.strip
+    description = node.search('.field-items > .field-item').text.strip
+    photo = node.search('.teaser-item__image img').text.strip
+
+    recipe = Recipe.new()
+    recipe.title = title
+    recipe.cooking_time = cooking_time
+    recipe.difficulty = difficulty
+    recipe.description = description
+    recipe.photo = photo
+    recipe.user = bob
+    recipe.save!
+  end
+end
