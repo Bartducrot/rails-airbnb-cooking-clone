08df7925dbdbb113c1f15e87081f0fc9e9e19069
Michael Belhassen <mikou12345@gmail.com>
2017-06-01 17:54:11 +0100

    Sexy admin: OSS 117

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Transaction.destroy_all
+Recipe.destroy_all
+User.destroy_all
+
+admin = User.create(first_name: "administration", last_name: "OSS 117", email: "hubert@gmail.com", password: "123456")
+
+1.times do |i|
+    url = "https://www.bbcgoodfood.com/search/recipes?query=&op=Search#page=#{i}"
+    html = Nokogiri::HTML(open(url))
+
+    ingredients = ["french"]
+    base_url = "https://www.bbcgoodfood.com"
+    ingredients.each do |ingredient|
+      url = "https://www.bbcgoodfood.com/search/recipes?query=#{ingredient}#query=#{ingredient}&page=#{i}"
+      url = "https://www.bbcgoodfood.com/search/recipes?query=#{ingredient}"
+      html = Nokogiri::HTML(open(url))
+
+      html.search('.node-recipe').each do |node|
+        title = node.search('h3 a').text.strip
+        cooking_time = node.search('.teaser-item__info-item--total-time').text.strip
+        difficulty = node.search('.teaser-item__info-item--skill-level').text.strip
+        description = node.search('.field-items > .field-item').text.strip
+
+        show = base_url + node.search('a').attr('href').text.strip
+        show_html = Nokogiri::HTML(open(show))
+        photo_max = show_html.search(".img-container img").attr('src').text.strip
+        instructions = show_html.search('.method__list p').text.strip
+
+        recipe = Recipe.new()
+        recipe.title = title
+        recipe.cooking_time = cooking_time
+        recipe.difficulty = difficulty
+        recipe.description = description
+        recipe.instructions = instructions
+        recipe.remote_photo_url = "https:" + photo_max if photo_max
+        recipe.user = admin
+        recipe.save!
+        puts "hacked recipe #{recipe.title}"
+      end
+    end
+end
