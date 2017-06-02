20058992020a2204954685e226469b200c22b9aa
Barthélémy Ducrot <barthelemy.ducrot@hei.fr>
2017-06-02 15:07:05 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,7 +45,7 @@ admin.save
         recipe.title = title
         recipe.cooking_time = cooking_time
         recipe.difficulty = difficulty
-        recipe.price = rand(1..2)
+        recipe.price = rand(1..5)
         recipe.description = description
         recipe.instructions = instructions
         recipe.remote_photo_url = "https:" + photo_max if photo_max
